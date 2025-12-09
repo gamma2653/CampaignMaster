b9df0f141d3c7e3d0325f94fd4c03b61f903e5ab
Christopher De Jesus <christopher2653@gmail.com>
2025-12-09 15:49:14 -0500

    Nesting in tanstack is hard.

--- campaign_master/web/react/features/shared/components/ctx.tsx
+++ campaign_master/web/react/features/shared/components/ctx.tsx
@@ -1,5 +1,38 @@
 import { createFormHookContexts, createFormHook } from '@tanstack/react-form'
+import { TextField, IDField, SubscribeButton, CampaignPlanField, PointField, SegmentField, ArcField } from './fields'
 
 export const { fieldContext, useFieldContext, formContext, useFormContext } =
     createFormHookContexts()
 
+
+export const { useAppForm, withForm, withFieldGroup } = createFormHook({
+    fieldComponents: {
+        /**
+         * A text input field component with label and error display.
+         * @param param0  Object containing the label for the field.
+         * @returns A JSX element representing the text field.
+         */
+        TextField,/**
+        * A field component for editing ID objects with prefix and numeric parts.
+        * @param param0  Object containing the label for the field.
+        * @returns A JSX element representing the ID field.
+        */
+        IDField,
+        PointField,
+        SegmentField,
+        ArcField,
+        CampaignPlanField,
+    },
+    formComponents: {
+        /**
+         * A button component that subscribes to the form's submitting state.
+         * @param param0  Object containing the label for the button.
+         * @returns A JSX element representing the subscribe button.
+         */
+        SubscribeButton,
+    },
+    fieldContext,
+    formContext,
+})
+
+export type useAppForm_RT = ReturnType<typeof useAppForm>
--- campaign_master/web/react/features/shared/components/forms.tsx
+++ campaign_master/web/react/features/shared/components/forms.tsx
@@ -1,98 +1,23 @@
-import { createFormHook, formOptions } from '@tanstack/react-form'
-import type { Arc, CampaignPlan, Point, Rule, Segment } from '../../../schemas'
-import { PREFIXES } from '../../../schemas'
-import { TextField, SubscribeButton, IDField, PointField, CampaignPlanField, arcOpts, campaignOpts } from './fields'
-import { fieldContext, formContext } from './ctx'
+import { formOptions, createFieldMap } from '@tanstack/react-form'
+import { useAppForm, withForm } from './ctx'
+import { defaultCampaignPlanValues } from './fields'
 
 
-const { useAppForm, withForm } = createFormHook({
-    fieldComponents: {
-        /**
-         * A text input field component with label and error display.
-         * @param param0  Object containing the label for the field.
-         * @returns A JSX element representing the text field.
-         */
-        TextField,/**
-        * A field component for editing ID objects with prefix and numeric parts.
-        * @param param0  Object containing the label for the field.
-        * @returns A JSX element representing the ID field.
-        */
-        IDField,
-        PointField,
-        CampaignPlanField,
-    },
-    formComponents: {
-        /**
-         * A button component that subscribes to the form's submitting state.
-         * @param param0  Object containing the label for the button.
-         * @returns A JSX element representing the subscribe button.
-         */
-        SubscribeButton,
-    },
-    fieldContext,
-    formContext,
-})
-
-export type useAppForm_RT = ReturnType<typeof useAppForm>
-
-
-
-
-
-export const ArcForm = () => {
-    const form = useAppForm({
-        ...arcOpts,
-    })
-    return (
-        <div>
-            <h1>Arc</h1>
-            <form.AppField
-                name="obj_id"
-                children={(field) => <field.IDField label="Arc ID" prefix={PREFIXES.ARC} />}
-            />
-            <form.AppField
-                name="name"
-                children={(field) => <field.TextField label="Arc Name" />}
-            />
-            <form.AppField
-                name="description"
-                children={(field) => <field.TextField label="Arc Description" />}
-            />
-            <form.AppField
-                name="segments"
-                mode="array"
-                children={(field) => (
-                    <div>
-                        {field.state.value.map((_, i) => (
-                            <div key={i}>
-                                <h2>Segment {i + 1}</h2>
-                                {/* <SegmentFormFields /> */}
-                            </div>
-                        ))}
-                    </div>
-                )}
-            />
-        </div>
-    )
-}
+// export const campaignPlanOpts = formOptions(campaignPlanOpts)
 
 export const CampaignPlanForm = () => {
     const form = useAppForm({
-        ...campaignOpts,
+        defaultValues: {campaign_plan: defaultCampaignPlanValues},
+        onSubmit: ({ value }) => console.log(`Submitting: '${value}'`)
     })
+    console.log('CampaignForm defaultValues:', defaultCampaignPlanValues)
 
     return (
-        <div>
-            <h1>Campaign Plan</h1>
-            {/* Add CampaignPlanField */}
-            <form.AppField
-                name="campaign_plan"
-                children={(field) => <field.CampaignPlanField />}
-            />
-            <form.AppForm>
-                <form.SubscribeButton label="Submit" />
-            </form.AppForm>
-        </div>
+        <form.AppField
+            name="campaign_plan"
+            children={(field) => <field.CampaignPlanField label="Campaign Plan" />}
+        >
+
+        </form.AppField>
     )
 }
-
